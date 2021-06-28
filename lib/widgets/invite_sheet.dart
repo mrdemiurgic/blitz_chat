fdc518e45d917e5bd72d8367f16c1f74b74d305f
Gabe Campbell <gabe@demiurgic.dev>
2021-06-28 16:30:28 -0500

    iOS universal link support

--- lib/widgets/invite_sheet.dart
+++ lib/widgets/invite_sheet.dart
@@ -10,42 +10,43 @@ class InviteSheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final roomName = BlocProvider.of<RoomNameCubit>(context).state;
-    final url = 'https://blitz.chat/#/$roomName';
+    final url = 'https://blitz.chat/$roomName';
 
     return Container(
+        color: Colors.amber,
         child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Text("Invite people into this room"),
-        Container(
-            padding: EdgeInsets.all(10.0),
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(50),
-            ),
-            child:
-                Text(roomName, style: Theme.of(context).textTheme.headline6)),
-        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          ElevatedButton.icon(
-              style: ButtonStyle(),
-              icon: Icon(Icons.share),
-              label: Text("Share"),
-              onPressed: () {
-                Navigator.pop(context);
-                Share.share(url);
-              }),
-          ElevatedButton.icon(
-              icon: Icon(Icons.link),
-              label: Text("Copy URL"),
-              onPressed: () {
-                Navigator.pop(context);
-                Clipboard.setData(new ClipboardData(text: url)).then((_) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text("URL copied to clipboard")));
-                });
-              })
-        ])
-      ],
-    ));
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("Invite people into this room"),
+            Container(
+                padding: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Text(roomName,
+                    style: Theme.of(context).textTheme.headline6)),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+              ElevatedButton.icon(
+                  style: ButtonStyle(),
+                  icon: Icon(Icons.share),
+                  label: Text("Share"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Share.share(url);
+                  }),
+              ElevatedButton.icon(
+                  icon: Icon(Icons.link),
+                  label: Text("Copy URL"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Clipboard.setData(new ClipboardData(text: url)).then((_) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text("URL copied to clipboard")));
+                    });
+                  })
+            ])
+          ],
+        ));
   }
 }
